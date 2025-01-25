74b70404ebc125f0d2838098fab56e6ec7856de5
Mani-420 <tahirabdulrehman46@gmail.com>
2025-01-26 00:07:34 +0500

    Product Components

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,9 +1,14 @@
 import "./Product.css";
+import Price from "./Price";
 
-function Product() {
+function Product({title, idx}) {
+    let oldPrice = ["15,000", "150", "5,000", "75,000"];
+    let newPrice = ["12,999", "99", "3,999", "59,999"];
   return (
     <div className="Product">
-        <p>Product</p>
+        <h4>{title}</h4>
+        <p>Description</p>
+        <Price oldPrice={oldPrice[idx]} newPrice={newPrice[idx]} />
     </div>
   );
 }
--- basic-react-app/src/ProductTab.jsx
+++ basic-react-app/src/ProductTab.jsx
@@ -3,10 +3,10 @@ import Product from "./Product.jsx";
 function ProductTab() {
   return (
     <>
-      <Product/>
-      <Product/>
-      <Product/>
-      <Product/>
+      <Product title="Gaming Controller" idx = {0} />
+      <Product title="Electric Pens" idx = {1}/>
+      <Product title="Apple Dots" idx = {2}/>
+      <Product title="Laptops" idx = {3}/>
     </>
   );
 }
